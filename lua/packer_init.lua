9f006ef7681ce4b658f9b52de894ed32a1c6f7d6
Calder Johnson <calderbjohnson@icloud.com>
2022-10-17 22:26:04 -0400

    Nvim conf

---
+++ lua/packer_init.lua
@@ -0,0 +1,171 @@
+-----------------------------------------------------------
+-- Plugin manager configuration file
+-----------------------------------------------------------
+
+-- Plugin manager: packer.nvim
+-- url: https://github.com/wbthomason/packer.nvim
+
+-- For information about installed plugins see the README:
+-- neovim-lua/README.md
+-- https://github.com/brainfucksec/neovim-lua#readme
+
+
+-- Automatically install packer
+local fn = vim.fn
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({
+    'git',
+    'clone',
+    '--depth',
+    '1',
+    'https://github.com/wbthomason/packer.nvim',
+    install_path
+  })
+  vim.o.runtimepath = vim.fn.stdpath('data') .. '/site/pack/*/start/*,' .. vim.o.runtimepath
+end
+
+-- Autocommand that reloads neovim whenever you save the packer_init.lua file
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost packer_init.lua source <afile> | PackerSync
+  augroup end
+]]
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, 'packer')
+if not status_ok then
+  return
+end
+
+-- Install plugins
+return packer.startup(function(use)
+  -- Add you plugins here:
+  use 'wbthomason/packer.nvim' -- packer can manage itself
+
+  -- File explorer
+  use 'kyazdani42/nvim-tree.lua'
+
+  -- Indent line
+  use 'lukas-reineke/indent-blankline.nvim'
+
+  -- Autopair
+  use {
+    'windwp/nvim-autopairs',
+    config = function()
+      require('nvim-autopairs').setup{}
+    end
+  }
+
+  -- Icons
+  use 'kyazdani42/nvim-web-devicons'
+
+  -- Tag viewer
+  use 'preservim/tagbar'
+
+  -- Treesitter interface
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+  }
+
+  -- Color schemes
+
+  use 'shaunsingh/nord.nvim'
+  use 'navarasu/onedark.nvim'
+  use 'tanvirtin/monokai.nvim'
+  use { 'rose-pine/neovim', as = 'rose-pine' }
+
+  -- LSP
+  use 'neovim/nvim-lspconfig'
+
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+  -- or                            , branch = '0.1.x',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  -- Autocomplete
+  use {
+    'hrsh7th/nvim-cmp',
+    requires = {
+      'L3MON4D3/LuaSnip',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-buffer',
+      'saadparwaiz1/cmp_luasnip',
+    },
+  }
+  use {'tzachar/cmp-tabnine', run='./install.sh', requires = 'hrsh7th/nvim-cmp'}
+  require'cmp'.setup {
+    sources = {
+      {name = 'cmp_tabnine'}
+    },
+  }
+
+  -- Statusline
+  use {
+    'feline-nvim/feline.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons' },
+  }
+
+  use {
+      'numToStr/Comment.nvim',
+      config = function()
+          require('Comment').setup()
+      end
+  }
+
+  -- git labels
+  use {
+    'lewis6991/gitsigns.nvim',
+    requires = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('gitsigns').setup{}
+    end
+  }
+
+  -- Dashboard (start screen)
+  use {
+    'goolord/alpha-nvim',
+    requires = { 'kyazdani42/nvim-web-devicons' },
+  }
+
+  -- Discord rich presence
+  use {
+    'andweeb/presence.nvim',
+    require("presence"):setup({
+    -- General options
+    auto_update         = true,
+    neovim_image_text   = "Neovim",
+    main_image          = "file",
+    client_id           = "793271441293967371",
+    log_level           = nil,
+    debounce_timeout    = 10,
+    enable_line_number  = true,
+    blacklist           = {},
+    buttons             = true,
+    file_assets         = {},
+    show_time           = true,
+
+    -- Rich Presence text options
+    editing_text        = "Editing %s",
+    file_explorer_text  = "Browsing %s",
+    git_commit_text     = "Committing changes",
+    plugin_manager_text = "Managing plugins",
+    reading_text        = "Reading %s",
+    workspace_text      = "Working on %s",
+    line_number_text    = "Line %s out of %s",
+    })
+  }
+
+
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
